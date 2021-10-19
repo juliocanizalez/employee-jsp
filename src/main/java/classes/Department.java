96306a8322d8ac0548c4e716234f6c98a1c0dcb2
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-19 11:30:04 -0600

    :hammer: Add department model

---
+++ src/main/java/classes/Department.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Department {
+  private int departmentNumber;
+  private String departmentName, location;
+
+  public int getDepartmentNumber() {
+    return departmentNumber;
+  }
+
+  public void setDepartmentNumber(int departmentNumber) {
+    this.departmentNumber = departmentNumber;
+  }
+
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+}
