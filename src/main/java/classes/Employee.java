fb12e9f3ff69e4699e6b1ee14c3983950ca40c90
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-19 11:30:35 -0600

    :hammer: Add Employee model

---
+++ src/main/java/classes/Employee.java
@@ -0,0 +1,78 @@
+package classes;
+
+public class Employee {
+  private int dir, employeeNumber;
+  private String lastName, position, date, salary, commission, departmentNumber, department;
+
+  public int getDir() {
+    return dir;
+  }
+
+  public void setDir(int dir) {
+    this.dir = dir;
+  }
+
+  public int getEmployeeNumber() {
+    return employeeNumber;
+  }
+
+  public void setEmployeeNumber(int employeeNumber) {
+    this.employeeNumber = employeeNumber;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public void setPosition(String position) {
+    this.position = position;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getSalary() {
+    return salary;
+  }
+
+  public void setSalary(String salary) {
+    this.salary = salary;
+  }
+
+  public String getCommission() {
+    return commission;
+  }
+
+  public void setCommission(String commission) {
+    this.commission = commission;
+  }
+
+  public String getDepartmentNumber() {
+    return departmentNumber;
+  }
+
+  public void setDepartmentNumber(String departmentNumber) {
+    this.departmentNumber = departmentNumber;
+  }
+
+  public String getDepartment() {
+    return department;
+  }
+
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+}
