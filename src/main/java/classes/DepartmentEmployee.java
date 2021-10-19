05a263c2795059a535cf80962f9e8736e4dd9ee7
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-19 11:32:23 -0600

    :hammer: Add merge classes

---
+++ src/main/java/classes/DepartmentEmployee.java
@@ -0,0 +1,22 @@
+package classes;
+
+public class DepartmentEmployee {
+  private int quantity;
+  private double avgSalary;
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public double getAvgSalary() {
+    return avgSalary;
+  }
+
+  public void setAvgSalary(double avgSalary) {
+    this.avgSalary = avgSalary;
+  }
+}
