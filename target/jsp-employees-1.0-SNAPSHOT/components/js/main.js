0e1e8085d7f4fc15d2b0b8ef7781e26d5591dfb4
Julio Canizalez <juliocanizalez@outlook.com>
2021-10-20 09:44:02 -0600

    :camera_flash: Update snapshot

--- target/jsp-employees-1.0-SNAPSHOT/components/js/main.js
+++ target/jsp-employees-1.0-SNAPSHOT/components/js/main.js
@@ -3,11 +3,24 @@ const fetchDepartment = () => {
 
   $.ajax({
     type: "POST",
-    url: "components/fetchDepartment.jsp",
+    url: "handlers/fetchDepartment.jsp",
     data: `departmentId=${departmentId}`,
     cache: false,
     success: response => {
       $("#data").html(response)
     }
+  })
+}
+
+const saveDepartment = () => {
+  const number = $("#departmentNumber").val();
+  const name = $("#departmentName").val();
+  const loc = $("#location").val();
+
+  $.ajax({
+    type: 'POST',
+    url: 'handlers/saveDepartment.jsp',
+    data: `departmentNumber=${number}&departmentName=${name}&location=${loc}`,
+    cache: false,
   })
 }
